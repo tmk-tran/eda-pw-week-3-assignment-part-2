de561337ec275942da214e40cef47f1dae5bd994
T Mark <tmk1.tran@gmail.com>
2023-07-31 15:47:50 -0500

    save

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -34,6 +34,17 @@ for ( let i = 3; i < 6; i++ ) {
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
 
+let count = 0;
+
+for ( let i = 2; i <= 10; i += 2 ) {
+  count++;
+  console.log( i );
+}
+
+
+
+
+
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
 
