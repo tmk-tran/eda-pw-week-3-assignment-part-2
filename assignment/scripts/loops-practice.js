92204eb8ba75ea1b07a56fd1c92d58ce4a29508e
T Mark <tmk1.tran@gmail.com>
2023-07-31 15:24:28 -0500

    started assignment part 2

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -14,9 +14,21 @@ for (let i = 0; i < 4; i++) {
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
 
+for ( let i = 0; i < 6; i++ ) {
+  console.log( i );
+}
+
+
+
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
+
+for ( let i = 3; i < 6; i++ ) {
+  console.log( i );
+}
+
+
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
